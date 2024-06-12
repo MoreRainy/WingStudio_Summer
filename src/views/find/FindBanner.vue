1a8c0bb704004566a3511e4443c8d30f5b66e99b
XYH <xyh1020642083@outlook.com>
2024-06-12 19:54:09 +0800

    轮播图基本完成

--- src/views/find/FindBanner.vue
+++ src/views/find/FindBanner.vue
@@ -9,7 +9,7 @@ let banners_first = ref('first') //字符串：作为无缝切换的克隆对象
 let banners_last = ref('last') //字符串：作为无缝切换的克隆对象
 
 let picCurrent = ref(1) //数组：记录当前轮播图,默认1，为数组第一张图片
-let picPosition = ref('') //字符串：代表ul位置，直接使用模板字符串修改translate享受不了响应式
+let picPosition = ref() //数字：表示图片位置
 
 let transitionFlag = ref(1) //判断是否执行过渡，1执行过渡，0不执行过渡
 
@@ -24,7 +24,7 @@ const timeLag = 5000
 const transitionLag = 0.5
 
 //轮播图初始值
-picPosition.value = `translateX(${-picCurrent.value * picWidth.value}px)`
+picPosition.value = -picCurrent.value * picWidth.value
 
 //清除动画函数，用于无缝衔接
 const clearTransition = () => {
@@ -49,6 +49,11 @@ onMounted(async () => {
 let startX = ref(0) //获取初始落点
 let moveX = ref(0) //计算移动距离
 let timer = ref() //申明定时器
+let recordX = ref(0) //用于记录上次滑动距离，用于恢复过渡未完成再次点击的距离，以防闪现
+//对recordX的补充说明：①他用于记录最后一次手动操作轮播图的位移
+//②若发生过渡未完成而再次点击，判定为前一次未中断的连续点击，他将用于恢复上一次的操作状态
+//③恢复状态后，他将修改为本次初始状态，避免两次点击期间因过渡产生的位移的影响
+
 //注册定时器函数
 const setTimer = () => {
   timer.value = setInterval(() => {
@@ -69,25 +74,56 @@ onUnmounted(clearTimer)
 //获取初始落点
 const moveIn = (e) => {
   startX.value = e.targetTouches[0].pageX
+  if (recordX.value) {
+    //如果切图了，复原
+    if (Math.abs(recordX.value) > picWidth.value * 0.3) {
+      picCurrent.value +=
+        (recordX.value / Math.abs(recordX.value)) *
+        Math.floor(
+          (Math.abs(recordX.value) - picWidth.value * 0.3) / picWidth.value + 1
+        )
+    }
+    //过渡未结束就点击，更改起始位置
+    const newPosition =
+      document.querySelector('ul').offsetLeft +
+      picCurrent.value * picWidth.value
+    recordX.value = newPosition //修改记录点到起始位置
+    moveX.value = newPosition //更改起始位置
+  }
   transitionFlag.value = 0 //禁用过渡标识，不然轮播图会不跟手，延迟！
   clearTimer() //禁用自动轮播
   clearTransition() //清除过渡
 }
+
 //移动跟随
 const moveIng = (e) => {
-  moveX.value = e.targetTouches[0].pageX - startX.value
+  if (recordX.value) {
+    moveX.value = e.targetTouches[0].pageX - startX.value + recordX.value
+  } else {
+    moveX.value = e.targetTouches[0].pageX - startX.value
+  }
 }
+
 //判读是否拉动足够的距离（30%）
 const moveOut = () => {
   if (Math.abs(moveX.value) > picWidth.value * 0.3) {
-    picCurrent.value -= moveX.value / Math.abs(moveX.value)
+    //用户一次可以滑过多个轮播图
+    //头真疼吧，这串代码达到了想要的预期，但是不知道该如何写注释，我很怀疑以后还看不看得懂
+    picCurrent.value -=
+      (moveX.value / Math.abs(moveX.value)) *
+      Math.floor(
+        (Math.abs(moveX.value) - picWidth.value * 0.3) / picWidth.value + 1
+      )
   }
-  moveX.value = 0 //清空
+  recordX.value = moveX.value //记录最后的距离
+  moveX.value = 0 //清空,此处清空moveX可达到回滚效果
   transitionFlag.value = 1
   setTimer()
 }
+
 const transitionEnd = () => {
   transitionFlag.value = 1
+  recordX.value = null //过渡完成，表示没有连续滑动轮播图，可清除
   clearTransition()
   //无缝切换处理
   if (picCurrent.value === banners_num.value - 1) {
@@ -104,6 +140,7 @@ const transitionEnd = () => {
     }, transitionLag * 1000)
   }
 }
+
 //解释：template中使用translateX会报错，故使用模板字符串
 watch([picCurrent, moveX], () => {
   //可用动画的情况：滑动不足30%回滚，滑动超过30%切图，
@@ -111,7 +148,7 @@ watch([picCurrent, moveX], () => {
   if (transitionFlag.value) {
     setTransition()
   }
-  picPosition.value = `translateX(${-picCurrent.value * picWidth.value + moveX.value}px)`
+  picPosition.value = -picCurrent.value * picWidth.value + moveX.value
 })
 </script>
 
@@ -125,7 +162,7 @@ watch([picCurrent, moveX], () => {
         ref="ul"
         :style="{
           width: `${100 * banners_num}%`,
-          transform: picPosition
+          left: `${picPosition}px`
         }"
         @touchstart="moveIn"
         @touchmove="moveIng"
@@ -166,12 +203,14 @@ watch([picCurrent, moveX], () => {
     height: 100%;
     overflow: hidden;
     border-radius: 10px;
+    position: relative;
 
     ul {
       display: flex;
       height: 100%;
       overflow: hidden;
       transition: none;
+      position: absolute;
 
       li {
         height: 100%;
